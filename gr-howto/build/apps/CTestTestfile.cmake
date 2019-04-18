f9e2175a157c8ff37f381eee7ef867a6e942126a
rwarner <rwarner@mymail.mines.edu>
2019-04-17 23:17:02 -0600

    Added a module example

---
+++ gr-howto/build/apps/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/rob/gr-howto/apps
+# Build directory: /home/rob/gr-howto/build/apps
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ gr-howto/build/include/howto/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/rob/gr-howto/include/howto
+# Build directory: /home/rob/gr-howto/build/include/howto
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
