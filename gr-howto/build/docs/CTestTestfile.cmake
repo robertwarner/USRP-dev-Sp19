78b914de458658b9d8fa0220819af7a7dcbb81e5
Ty <tychristensen97@gmail.com>
2019-05-10 09:04:36 -0700

    Reorganized

--- gr-howto/build/docs/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/rob/gr-howto/docs
-# Build directory: /home/rob/gr-howto/build/docs
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("doxygen")
